b430b4e3ade38fb67eb0d866cd824e18aaf6bd10
KIM CHUNG IL <cnddlf12@naver.com>
2023-06-24 20:47:55 +0900

    3-41

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Todo from './Todo';
+import { Container, List, Paper } from "@mui/material";
+import AddTodo from './AddTodo';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  const addItem = (item) => {
+    item.id = "ID-" + items.length; // key를 위한 id
+    item.done = false // done 초기화
+    // 업데이트는 반드시 setItems로 하고 새 배열을 만들어야 한다.
+    setItems([...items, item]);
+    console.log("items : ". items);
+  }
+
+  const deleteItem = (item) => {
+    // 삭제할 아이템을 찾는다.
+    const newItems = items.filter(e => e.id !== item.id);
+    // 삭제할 아이템을 제외한 아이템을 다시 배열에 저장한다.
+    setItems([...newItems]);
+  }
+
+  const editItem = () => {
+    setItems([...items]);
+  }
+
+  let todoItems = items.length > 0 && (
+    <Paper style={{margin: 16 }}>
+      <List>
+        {items.map((item) => (
+          <Todo
+            item={item}
+            key={item.id}
+            editItem={editItem}
+            deleteItem={deleteItem} />
+        ))}
+      </List>
+    </Paper>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='App'>
+          <Container maxWidth="md">
+              <AddTodo addItem={addItem} />
+              <div className="TodoList">{todoItems}</div>
+          </Container>  
+      </div>
   );
 }
 
